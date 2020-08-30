afd2ff8edc12fa1bea458dbebbc9f9b6e28d9d18
yuara <yu.arakawa112@gmail.com>
2020-08-30 09:50:32 +0900

    Created urls and views to add clothes and to get category

--- services/closet/urls.py
+++ services/closet/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = "closet"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.ClothesCreate.as_view(), name="add"),
+    path("api/category/get/", views.ajax_get_category, name="ajax_get_category"),
+]
--- services/closet/views.py
+++ services/closet/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views import generic
+from .forms import ClothesCreateForm
+from .models import ParentCategory, Category, Clothes
+from django.urls import reverse_lazy
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+class ClothesCreate(generic.CreateView):
+    model = Clothes
+    form_class = ClothesCreateForm
+    success_url = reverse_lazy("account:current")
+    template_name = "closet/add_clothes.html"
+
+
+def ajax_get_category(request):
+    pk = request.GET.get("pk")
+    # Return all categories if pk is None or empty.
+    if not pk:
+        category_list = Category.objects.all()
+
+    # Return categories if geting its pk
+    else:
+        category_list = Category.objects.filter(parent__pk=pk)
+
+    # Return a list that has dicts like this [ {'name': 'サッカー', 'pk': '3'}, {...}, {...} ]
+    category_list = [
+        {"pk": category.pk, "name": category.name} for category in category_list
+    ]
+
+    # Return as JSON
+    return JsonResponse({"categoryList": category_list})
