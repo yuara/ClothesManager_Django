210c5fb6d29c095f8699680647e7ffa5d7101763
yuara <yu.arakawa112@gmail.com>
2020-08-30 21:37:43 +0900

    Created urls for clothes

--- services/closet/urls.py
+++ services/closet/urls.py
@@ -4,6 +4,11 @@ from . import views
 app_name = "closet"
 
 urlpatterns = [
-    path("", views.ClothesCreate.as_view(), name="add"),
+    path("clothes/", views.UserClothes.as_view(), name="clothes"),
+    path("clothes/add", views.ClothesCreate.as_view(), name="add"),
+    path("clothes/<int:pk>/edit", views.EditClothes.as_view(), name="edit_clothes"),
+    path(
+        "clothes/<slug:username>/", views.LimitedClothes.as_view(), name="user_clothes"
+    ),
     path("api/category/get/", views.ajax_get_category, name="ajax_get_category"),
 ]
