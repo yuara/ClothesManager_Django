bbeed9cbdae27fe79cbc50dccb7dc25b9135f062
yuara <yu.arakawa112@gmail.com>
2020-08-30 09:54:21 +0900

    Created signals.py to populate category data

--- services/closet/apps.py
+++ services/closet/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class ClosetConfig(AppConfig):
-    name = 'closet'
+    name = "closet"
+
+    def ready(self):
+        from .signals import create_default_category
+
+        post_migrate.connect(create_default_category, sender=self)
