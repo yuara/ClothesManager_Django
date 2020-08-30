68e37ea2d84083e00808012890a9a41e832aa08e
yuara <yu.arakawa112@gmail.com>
2020-08-30 12:33:26 +0900

    Clothes admin with parent and category

--- services/closet/admin.py
+++ services/closet/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.utils.translation import gettext, gettext_lazy as _
+from .models import ParentCategory, Category, Clothes
 
 # Register your models here.
+
+
+@admin.register(ParentCategory)
+class ParentCategoryAdmin(admin.ModelAdmin):
+    ordering = ("pk",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ordering = ("pk",)
+
+
+@admin.register(Clothes)
+class ClothesAdmin(admin.ModelAdmin):
+    pass
