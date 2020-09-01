0f61bdca92f4a07e57078b865ec8be8b79814552
yuara <yu.arakawa112@gmail.com>
2020-09-01 21:56:41 +0900

    modified name of url of PublishedOutfit

--- services/closet/urls.py
+++ services/closet/urls.py
@@ -16,7 +16,9 @@ urlpatterns = [
     path("outfit/set", views.CreateOutfit.as_view(), name="set"),
     path("outfit/<int:pk>/edit", views.EditOutfit.as_view(), name="edit_outfit"),
     path(
-        "outfit/<slug:username>/", views.PublishedOutfits.as_view(), name="user_outfit",
+        "outfit/<slug:username>/",
+        views.PublishedOutfits.as_view(),
+        name="user_outfits",
     ),
     path("api/category/get/", views.ajax_get_category, name="ajax_get_category"),
 ]
