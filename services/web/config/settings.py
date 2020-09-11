0185de5f340afbf0b2d29461c6b339963e79aa94
yuara <yu.arakawa112@gmail.com>
2020-09-11 09:46:42 +0900

    Created container for dev

--- services/web/config/settings.py
+++ services/web/config/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
@@ -118,11 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-MEDIA_ROOT = BASE_DIR / "staticfiles" / "media_root"
-MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+MEDIA_URL = "/mediafiles/"
 
-STATIC_ROOT = BASE_DIR / "staticfiles" / "static_root"
-STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/staticfiles/"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
