d8a3d86d493527b90fddb1297a20ee90ca5e66bd
yuara <yu.arakawa112@gmail.com>
2020-09-15 16:08:46 +0900

    Add models to admin

--- services/web/closet/admin.py
+++ services/web/closet/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from django.utils.translation import gettext, gettext_lazy as _
-from .models import ParentCategory, Category, Clothes, Outfit
+from .models import (
+    ParentCategory,
+    Category,
+    Clothes,
+    Outfit,
+    Area,
+    Prefecture,
+    ClothesIndex,
+    Weather,
+    Forecast,
+)
 
 # Register your models here.
 
@@ -22,4 +32,29 @@ class ClothesAdmin(admin.ModelAdmin):
 
 @admin.register(Outfit)
 class OutfitAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Area)
+class AreaAdmin(admin.ModelAdmin):
+    ordering = ("pk",)
+
+
+@admin.register(Prefecture)
+class PrefectureAdmin(admin.ModelAdmin):
+    ordering = ("pk",)
+
+
+@admin.register(ClothesIndex)
+class ClothesIndexAdmin(admin.ModelAdmin):
+    ordering = ("pk",)
+
+
+@admin.register(Weather)
+class WeatherAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Forecast)
+class ForecastAdmin(admin.ModelAdmin):
     pass
