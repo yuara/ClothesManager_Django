145bacfe5dad3380bf09dd01c37851be08fd6d6f
yuara <yu.arakawa112@gmail.com>
2020-09-20 22:02:33 +0900

    Added following and followers fields in User admin

--- services/web/accounts/admin.py
+++ services/web/accounts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext, gettext_lazy as _
-from .models import User, Profile, FollowUser
+from .forms import UserChangeForm, ProfileForm
+from .models import User, Profile
 
 # Register your models here.
 
@@ -13,25 +15,28 @@ class ProfileInline(admin.StackedInline):
     can_delete = False
 
 
-class FollowingInline(admin.StackedInline):
-    model = FollowUser
-    fk_name = "following"
-    extra = 1
-
-
-class FollowerInline(admin.StackedInline):
-    model = FollowUser
-    fk_name = "follower"
-    extra = 1
+# class FollowingInline(admin.StackedInline):
+#     model = FollowUser
+#     fk_name = "following"
+#     extra = 1
+#
+#
+# class FollowerInline(admin.StackedInline):
+#     model = FollowUser
+#     fk_name = "follower"
+#     extra = 1
+#
 
 
 @admin.register(User)
 class AdminUserAdmin(BaseUserAdmin):
-    inlines = (ProfileInline, FollowerInline, FollowingInline)
+    inlines = (ProfileInline,)
+    form = UserChangeForm
+    model = get_user_model()
 
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("full_name", "email",)},),
+        (_("Personal info"), {"fields": ("full_name", "email")},),
         (
             _("Permissions"),
             {
@@ -45,6 +50,7 @@ class AdminUserAdmin(BaseUserAdmin):
             },
         ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        ("Following / Followers", {"fields": ("following", "followers")}),
     )
     list_display = (
         "username",
@@ -53,10 +59,7 @@ class AdminUserAdmin(BaseUserAdmin):
         "is_staff",
     )
     search_fields = ("username", "full_name", "email")
-    filter_horizontal = (
-        "groups",
-        "user_permissions",
-    )
+    filter_horizontal = ("groups", "user_permissions", "following", "followers")
 
 
 admin.site.register(Profile)
--- services/web/accounts/forms.py
+++ services/web/accounts/forms.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
+from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from .models import Profile
+from .models import User, Profile
 
 
 class UserCreateForm(UserCreationForm):
@@ -19,3 +20,45 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ("about_me", "webpage", "picture")
+
+
+class UserChangeForm(forms.ModelForm):
+
+    following = forms.ModelMultipleChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        widget=FilteredSelectMultiple(verbose_name="Following", is_stacked=False),
+    )
+
+    followers = forms.ModelMultipleChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        widget=FilteredSelectMultiple(verbose_name="Followers", is_stacked=False),
+    )
+
+    class Meta:
+        model = get_user_model()
+        # add 'following' and 'followers' to the fields:
+        fields = ("following", "followers")
+
+    # also needed to initialize properly:
+    def __init__(self, *args, **kwargs):
+        super(UserChangeForm, self).__init__(*args, **kwargs)
+
+        # Filter out the self user in the lists and initialize followers list:
+        if self.instance and self.instance.pk:
+            self.fields["following"] = forms.ModelMultipleChoiceField(
+                queryset=User.objects.all().exclude(pk=self.instance.pk),
+                required=False,
+                widget=FilteredSelectMultiple(
+                    verbose_name="Following", is_stacked=False
+                ),
+            )
+            self.fields["followers"] = forms.ModelMultipleChoiceField(
+                queryset=User.objects.all().exclude(pk=self.instance.pk),
+                required=False,
+                widget=FilteredSelectMultiple(
+                    verbose_name="Followers", is_stacked=False
+                ),
+            )
+            self.fields["followers"].initial = self.instance.followers.all()
