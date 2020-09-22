70296c19fc390704ab03ce58832dc1fe3ad03dab
yuara <yu.arakawa112@gmail.com>
2020-09-23 00:57:20 +0900

    Added whitenoise to requirements

--- services/web/config/settings.py
+++ services/web/config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -136,3 +137,5 @@ LOGIN_REDIRECT_URL = "index"
 LOGOUT_REDIRECT_URL = "thanks"
 
 AUTH_USER_MODEL = "accounts.User"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
