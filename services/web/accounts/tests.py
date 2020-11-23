0b96ba3a1c724ccf6f057bb3decf10b5532faaa9
yuara <yu.arakawa112@gmail.com>
2020-11-23 16:39:53 +0900

    Tested accounts

--- services/web/accounts/tests.py
+++ services/web/accounts/tests.py
@@ -6,30 +6,34 @@ class AccountsTests(TestCase):
     fixtures = ["areas", "prefectures"]
 
     def setUp(self):
-        self.u1 = User.objects.create(username="Harry")
-        self.u2 = User.objects.create(username="Ron")
-        self.u3 = User.objects.create(username="Hermione")
+        User.objects.create(username="test1")
+        User.objects.create(username="test2")
+        User.objects.create(username="test3")
 
     def test_password_hashing(self):
-        self.u1.set_password("test_password")
-        self.assertFalse(self.u1.check_password("something"))
-        self.assertTrue(self.u1.check_password("test_password"))
+        u = User.objects.get(username="test1")
+        u.set_password("test_password")
+        self.assertFalse(u.check_password("something"))
+        self.assertTrue(u.check_password("test_password"))
 
     def test_create_profile(self):
         u = User.objects.create(username="test_profile")
         self.assertTrue(u.profile)
-
+        self.assertTrue(u.profile.color)
         self.assertEqual(u.profile.area.pk, 1)
         self.assertEqual(u.profile.prefecture.pk, 1)
 
     def test_follow(self):
-        self.assertEqual(list(self.u1.following.all()), [])
+        t_1 = User.objects.get(username="test1")
+        t_2 = User.objects.get(username="test2")
+        t_3 = User.objects.get(username="test3")
+        self.assertEqual(list(t_1.following.all()), [])
 
-        self.u1.follow(self.u2)
-        self.assertTrue(self.u1.is_following(self.u2))
-        self.u1.follow(self.u3)
-        self.assertTrue(self.u1.is_following(self.u3))
-        self.assertEquals(list(self.u1.following.all()), [self.u2, self.u3])
+        t_1.follow(t_2)
+        self.assertTrue(t_1.is_following(t_2))
+        t_1.follow(t_3)
+        self.assertTrue(t_1.is_following(t_3))
+        self.assertEquals(list(t_1.following.all()), [t_2, t_3])
 
-        self.u1.unfollow(self.u3)
-        self.assertEquals(list(self.u1.following.all()), [self.u2])
+        t_1.unfollow(t_3)
+        self.assertEquals(list(t_1.following.all()), [t_2])
