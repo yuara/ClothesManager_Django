76e2d130491f2882d42dc3ce024817d737eee308
yuara <yu.arakawa112@gmail.com>
2020-11-23 17:26:43 +0900

    Test: config views

--- services/web/config/tests.py
+++ services/web/config/tests.py
@@ -1,127 +1,82 @@
 # from django.urls import resolve
-# from django.test import TestCase
+from django.urls import reverse
+from django.test import TestCase, Client
+from accounts.models import User, Profile
+import factory
+
 # from django.http import HttpRequest
 # from .views import HomePage
-#
-#
-# class HomePageTest(TestCase):
-# def test_root_url(self):
-#     found = resolve("/")
-#     self.assertEqual(found.func, HomePage.as_view())
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
 
-# def test_home_return_title_in_html(self):
-#     request = HttpRequest()
-#     response = HomePage.as_view(request)
-#     html = response.content.decode("utf8")
-#     self.assertTrue(html.startwith("<html>"))
-#     self.assertIn("<title>Home</title>", html)
-#     self.assertTrue(html.endwith("<html>"))
-
-# def test_home_return_title_in_html(self):
-#     request = self.client.get('/')
-#     html = response.content.decode("utf8")
-#     self.assertTrue(html.startwith("<html>"))
-#     self.assertIn("<title>Home</title>", html)
-#     self.assertTrue(html.strip().endwith("<html>"))
-
-# def test_home_template(self):
-#     response = self.client.get("/")
-#     self.assertTemplateUsed(response, "home.html")
-
-import os
-import socket
-import time
-import factory
-import unittest
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from django.conf import settings
-
-# from django.test import LiveServerTestCase
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.contrib.auth.models import User
-from closet.models import ClothesIndex
+    username = factory.Sequence(lambda x: f"user_{x}")
+    email = factory.LazyAttribute(lambda a: f"{a.username}@example.com")
 
 
-# class UserFactory(factory.django.DjangoModelFactory):
-#     FACTORY_FOR = User
-#
-#     # username = factory.Faker("first_name")
-#     # email = factory.LazyAttribute(lambda a: f"{a.username}@example.com")
-#     username = "yuarakawa"
-#     email = "yu.arakawa@example.com"
-#     password = factory.PostGenerationMethodCall("set_password", "admin_yuarakawa")
-#
-#     is_superuser = True
-#     is_staff = True
-#     is_active = True
+class ProfileFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Profile
 
-# os.environ["DJANGO_LIVE_TEST_SERVER_ADDRESS"] = "0.0.0.0:8000"
+    user = factory.SubFactory(UserFactory)
 
 
-class VisitTest(StaticLiveServerTestCase):
-    live_server_url = f"http://{socket.gethostbyname(socket.gethostname())}:8000"
+class AdminUserFactory(factory.django.DjangoModelFactory):
+    # FACTORY_FOR = User
+
+    # username = factory.Faker("first_name")
+    # email = factory.LazyAttribute(lambda a: f"{a.username}@example.com")
+    class Meta:
+        model = User
+
+    username = "yuarakawa"
+    email = "yu.arakawa@example.com"
+    password = factory.LazyFunction(lambda: make_password("admin_yuarakawa"))
+    profile = factory.RelatedFactory(
+        ProfileFactory, "user", area=1, prefecture=1, color="123456"
+    )
+
+    is_superuser = True
+    is_staff = True
+    is_active = True
+
+
+class HomePageTest(TestCase):
+    fixtures = ["areas", "prefectures"]
 
     def setUp(self):
-
-        settings.DEBUG = True
-        options = webdriver.ChromeOptions()
-        options.binary_location = "/usr/bin/google-chrome"
-        options.add_argument("--no-sandbox")
-        options.add_argument("--headless")
-        options.add_argument("--disable-gpu")
-        capabilities = options.to_capabilities()
-
-        self.browser = webdriver.Remote(
-            command_executor="http://chromedriver:9515",
-            desired_capabilities=capabilities,
+        u = User.objects.create(
+            username="test", email="test@example.com", password="test"
         )
 
-    def tesrDrop(self):
-        self.browser.quit()
+    # def test_root_url(self):
+    #     found = resolve("/")
+    #     self.assertEqual(found.func, HomePage.as_view())
+    #
+    # def test_home_return_title_in_html(self):
+    #     request = HttpRequest()
+    #     response = HomePage.as_view(request)
+    #     html = response.content.decode("utf8")
+    #     self.assertTrue(html.startwith("<html>"))
+    #     self.assertIn("<title>Home</title>", html)
+    #     self.assertTrue(html.endwith("<html>"))
 
-    def test_visit(self):
-        # Access the home page of this app
-        self.browser.get(self.live_server_url)
+    # def test_home_return_title_in_html(self):
+    #     request = self.client.get('/')
+    #     html = response.content.decode("utf8")
+    #     self.assertTrue(html.startwith("<html>"))
+    #     self.assertIn("<title>Home</title>", html)
+    #     self.assertTrue(html.strip().endwith("<html>"))
 
-        # Confirm a title and header
-        self.assertIn("Home", self.browser.title)
+    # def test_home_template(self):
+    #     response = self.client.get("/index/")
+    #     self.assertTemplateUsed(response, "home.html")
 
-    # header_text = self.browser.find_element_by_tag_name("h1").text
-    # self.assertIn("ClothesManager", header_text)
-    # self.fail("Fail........")
+    def test_login_root(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(reverse("index"))
+        html = response.content.decode("utf8")
+        print(f"html------------------------\n{html}")
 
-    # def test_login(self):
-    #     self.browser.get(self.live_server_url + "/accounts/login/")
-    #
-    #     body = self.browser.find_element_by_tag_name("body")
-    #     self.assertIn("Django administration", body.text)
-    #
-    #     username_field = self.browser.find_element_by_name("username")
-    #     username_field.send_keys(yu.username)
-    #     password_field = self.browser.find_element_by_name("password")
-    #     password_field.send_keys(yu.password)
-    #     password_field.send_keys(Keys.RETURN)
-    #
-    #     self.assertIn("Dashboard - ClothesManager", self.browser.title)
-    #
-    # def test_admin_login(self):
-    #     yu = UserFactory.create()
-    #
-    #     # Yu opens the browser and goes to the admin page
-    #     self.browser.get(self.live_server_url + "/admin/")
-    #
-    #     # Yu sees the familiar 'Django Administration' heading
-    #     body = self.browser.find_element_by_tag_name("body")
-    #     self.assertIn("Django administration", body.text)
-    #
-    #     # Yu types in his username and password and hits return
-    #     username_field = self.browser.find_element_by_name("username")
-    #     username_field.send_keys(yu.username)
-    #     password_field = self.browser.find_element_by_name("password")
-    #     password_field.send_keys(yu.password)
-    #     password_field.send_keys(Keys.RETURN)
-    #
-    #     # Yu finds himself on the 'Dashboard'
-    #     body = self.browser.find_element_by_tag_name("body")
-    #     self.assetrIn("Dashboard", body.text)
+        # self.assertTemplateUsed(response, "index.html")
+        self.assertIn("Dashboard", html)
