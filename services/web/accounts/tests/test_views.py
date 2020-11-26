88406f72869b83c13f9befb4e8a43d5b4a369709
yuara <yu.arakawa112@gmail.com>
2020-11-26 13:45:31 +0900

    Test url of sign up

---
+++ services/web/accounts/tests/test_views.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.urls import reverse
+from accounts.models import User
+
+
+class AccountsViewsTest(TestCase):
+    fixtures = ["areas", "prefectures"]
+
+    def setUp(self):
+        u1 = User.objects.create(username="test_user_1")
+        u1.set_password("testing_user_1_now")
+        u2 = User.objects.create(username="test_user_2")
+        u2.set_password("testing_user_2_now")
+        u1.save()
+        u2.save()
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse("accounts:signup"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse("accounts:signup"))
+        self.assertTemplateUsed(response, "accounts/signup.html")
